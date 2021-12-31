d57761380158e673de7963d60a65f7f6cc77dcb5
neonita <neonita888@gmail.com>
2021-12-30 21:32:21 -0500

    styled and added a footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
 
 function App() {
+  function change(govName) {
+    govName.target.innerHTML = `мεℓιssα вαησεη-gαя∂ε (◔◡◔)`;
+  }
+
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="/" className="primary-link">
+          <span
+            onMouseOver={change}
+            onMouseLeave={(displayedName) =>
+              (displayedName.target.innerHTML = `Neonita`)
+            }
+          >
+            Neonita
+          </span>
+        </a>
+        and is{" "}
+        <a href="/" target="noopener norefferer" className="primary-link">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
