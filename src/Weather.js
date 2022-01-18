db7fc9ae433285a7575c6ffd8ef557e28cb4d0e8
neonita <neonita888@gmail.com>
2022-01-18 01:02:02 -0500

    created new component for weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -73,6 +74,7 @@ export default function Weather(props) {
 
         {/* Component that displays the wheather info's data, passing the weatherData object */}
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   }
