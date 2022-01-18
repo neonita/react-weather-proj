6f5e26edda9732236f846854bf385ab60bbb73bc
neonita <neonita888@gmail.com>
2022-01-18 01:20:51 -0500

    structured the display of a unique forecast to later be looped through the api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,22 @@
 import React from "react";
+import FormattedIcons from "./FormattedIcons";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-  return <>hello</>;
+  return (
+    <div className="WeatherForecast">
+      <div className="row mt-3">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <div className="WeatherForecast-icon">
+            <FormattedIcons iconId="600" icon="13d" />
+          </div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19&#176;</span>
+            <span className="WeatherForecast-temperature-min">10&#176;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
